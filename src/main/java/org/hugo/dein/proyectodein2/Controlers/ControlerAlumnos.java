7d4b85dd7f9cc4d9f5d26ca138a8a4e73461955d
Hugojavid5 <hugojavidferrero@gmail.com>
2025-01-28 20:15:15 +0100

    ventana alumnos echa

---
+++ src/main/java/org/hugo/dein/proyectodein2/Controlers/ControlerAlumnos.java
@@ -0,0 +1,31 @@
+package org.hugo.dein.proyectodein2.Controlers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ControlerAlumnos {
+
+    @FXML
+    private TextField txt_dni;
+
+    @FXML
+    private TextField txt_nombre;
+
+    @FXML
+    private TextField txt_ape1;
+
+    @FXML
+    private TextField txt_ape2;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarAlumno(ActionEvent event) {
+
+    }
+
+}
