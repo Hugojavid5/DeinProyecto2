802f53934052d864a35c3a9d3c1191819e0c3d34
Hugojavid5 <hugojavidferrero@gmail.com>
2025-01-29 18:45:28 +0100

    arreglando cosas

--- src/main/java/org/hugo/dein/proyectodein/Controlers/ControlerPrestamo.java
+++ src/main/java/org/hugo/dein/proyectodein/Controlers/ControlerPrestamo.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import org.hugo.dein.proyectodein.Modelos.ModeloLibro;
 
 public class ControlerPrestamo {
 
@@ -19,7 +20,7 @@ public class ControlerPrestamo {
     private ComboBox<?> comboEstudiante;
 
     @FXML
-    private ComboBox<?> comboLibro;
+    private ComboBox<ModeloLibro> comboLibro;
 
     @FXML
     private DatePicker dpFechaPrestamo;
