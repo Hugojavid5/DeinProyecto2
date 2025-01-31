95ec47df4808f4951e745b6469244dac181e5050
Hugojavid5 <hugojavidferrero@gmail.com>
2025-02-01 00:14:39 +0100

    probando multi

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,20 @@
 module org.hugo.dein.proyectodein {
     requires javafx.controls;
     requires javafx.fxml;
-    requires cdi.api;
-    requires java.sql.rowset;
+    requires java.sql;
     requires org.slf4j;
     requires net.sf.jasperreports.core;
-    requires java.desktop;
+    requires cdi.api;
+
 
     opens org.hugo.dein.proyectodein to javafx.fxml;
-    opens org.hugo.dein.proyectodein.Modelos to javafx.base;
-    opens org.hugo.dein.proyectodein.Controlers to javafx.fxml;
-
     exports org.hugo.dein.proyectodein;
+
+    opens org.hugo.dein.proyectodein.Modelos to javafx.base;
+    exports org.hugo.dein.proyectodein.Modelos;
+
+    opens org.hugo.dein.proyectodein.Controlers to javafx.fxml;
+    exports org.hugo.dein.proyectodein.Controlers;
+
+
 }
